6367cc81992eac682fdd47c4f71f9bb6e5e7c400
Frank Laszlo <frank@franksworld.org>
2015-08-21 11:18:03 -0400

    asdf

--- nocworx/html/index.php
+++ nocworx/html/index.php
@@ -16,6 +16,7 @@ echo "2 = " . $num->getTwo() . "\n";
 echo "3 = " . $num->getThree() . "\n";
 echo "4 = " . $num->getFour() . "\n";
 echo "5 = " . $num->getFive() . "\n";
-echo "6 = ". $num->getSix() . "\n";
+echo "6 = " . $num->getSix() . "\n";
+echo "7 = " . $num->getSeven() . "\n";
 
 echo "</pre>";
