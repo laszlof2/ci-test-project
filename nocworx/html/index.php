95c511deb6e24c7f60515fbcbc4670eafc522857
Frank Laszlo <frank@franksworld.org>
2015-08-21 10:18:45 -0400

    Stuff

--- nocworx/html/index.php
+++ nocworx/html/index.php
@@ -13,5 +13,6 @@ echo "1 = " . $num->getOne() . "\n";
 echo "2 = " . $num->getTwo() . "\n";
 echo "3 = " . $num->getThree() . "\n";
 echo "4 = " . $num->getFour() . "\n";
+echo "5 = " . $num->getFive() . "\n";
 
 echo "</pre>";
