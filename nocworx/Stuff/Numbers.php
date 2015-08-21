1da125baf2561f0bdf0fff5171bf0cd8c2470bfc
Frank Laszlo <frank@franksworld.org>
2015-08-21 10:32:57 -0400

    fix it

--- nocworx/Stuff/Numbers.php
+++ nocworx/Stuff/Numbers.php
@@ -3,8 +3,8 @@
 namespace NocWorx\Stuff;
 
 class Numbers {
-  private $_one = 9999;
-  private $_two = 222;
+  private $_one = 1;
+  private $_two = 2;
   private $_three = 3;
   private $_four = 4;
   private $_five = 5;
